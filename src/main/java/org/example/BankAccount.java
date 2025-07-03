35c4e05165c01c7fb7a0763d4ede40e06c6bcfbf
ethan-josh <ethanjoshua.camingao@gcash.com>
2025-07-03 15:30:24 +0800

    feat: basic user display

---
+++ src/main/java/org/example/BankAccount.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class BankAccount {
+    public String accountNumber;
+    public String accountName;
+    public double availableBalance;
+
+    /*
+    Methods:
+    Deposit: Adds to balance (with error check)
+    Withdraw: Subtracts from balance (with error check)
+    Display Information:Prints account info
+    Get Account Number: Returns account number (used for searching)
+     */
+
+    public void deposit(){
+
+    }
+
+    public void withdraw(){
+
+    }
+
+    public void displayInformation(){
+
+    }
+
+    public void getAccountNumber(){
+
+    }
+
+}
---
+++ src/main/java/org/example/BankSystem.java
@@ -0,0 +1,21 @@
+package org.example;
+import java.util.*;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class BankSystem {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        int userChoice = 0;
+
+        while(userChoice != 6){
+            System.out.println("=== Bank Menu ===");
+            System.out.println("1. Create Account");
+            System.out.println("2. View All Accounts");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Deposit");
+            System.out.println("5. Withdraw");
+            System.out.println("6. Exit");
+            userChoice = myObj.nextInt();
+        }
+    }
+}
